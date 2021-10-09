f377da402471a5610c7fa66ba8098a868cbf4803
Elliot Kantor <elliotjkantor@gmail.com>
2021-10-09 18:41:11 -0400

    Read in string data from file

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import streamlit as st
 import pandas as pd
 from parse import DataParser
 import matplotlib.pyplot as plt
+from io import StringIO
+
 
 st.title("Pnut Altimeter Data Parser")
-file = st.file_uploader("Upload your pf2 file")
 
-parser = DataParser(file)
-parser.parse()
-df = parser.get_dataframe()
+file = st.file_uploader("Upload your pf2 file", ("pf2"))
 
-# graph it
-plt.plot(df.Time, df.Altitude)
+if file:
+    # get string from file
+    string_data = StringIO(file.getvalue().decode("utf-8")).read()
+    st.code(string_data)
+
+    # parser = DataParser(string_data)
+    # parser.parse()
+    # df = parser.get_dataframe()
+
+    # # graph it
+    # st.line_chart(data=df["Time", "Altitude"])
